01e6017d29d8300c7edf63080d2af437510fb25b
szililaszlo <laszlo0919@gmail.com>
2017-12-16 18:39:40 +0100

    #4

--- src/com/company/Sort.java
+++ src/com/company/Sort.java
@@ -2,16 +2,22 @@ package com.company;
 
 public class Sort implements ISort {
 
-    //buborék rendezés
+    //4
+    //buborék rendezés - Optimalizált!!
     public void bubbleSort(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
+            int swaps = 0;
             for (int j = 0; j <= i - 1; j++) {
                 int helper;
                 if (array[j] > array[j + 1]) {
                     helper = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = helper;
+                    swaps++;
                 }
+            }
+            if(swaps==0) {
+                break;
             }
         }
     }
